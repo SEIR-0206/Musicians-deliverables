9db82968cca2ca2217ebfc776ae74d74d5a7cfc9
wylietroy <wyliesebtroy@gmail.com>
2023-03-20 01:14:37 -0400

    home 404

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const musiciansController = require('./controllers/musicians');
+
+app.set('view engine', 'ejs');
+
+app.use('/musicians', musiciansController)
+
+app.get('/', (req, res) => {
+    res.render('home.ejs');
+})
+
+app.get('/*',  (req,res) => {
+    res.render('404.ejs');
+})
+
+app.listen(4000, () => {
+    console.log("Listening on port 4000");
+})
